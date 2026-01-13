6f1646f3ba6094b2795a1d7fc8a9659300cce8b1
wgrodesky <will.grodesky@kellogg.northwestern.edu>
2026-01-13 16:54:52 +0000

    update

--- lab-2-2.sql
+++ lab-2-2.sql
@@ -9,4 +9,5 @@
 -- | 2020 | Chicago White Sox |
 -- +------+-------------------+
 
+SELECT year, name FROM teams WHERE park = "Guaranteed Rate Field";
 
---
+++ schema.sql
@@ -0,0 +1,28 @@
+-- CREATE TABLE teams (
+--   id INTEGER PRIMARY KEY,
+--   year INTEGER,
+--   name TEXT,
+--   park TEXT,
+--   wins INTEGER,
+--   losses INTEGER
+-- );
+-- CREATE TABLE players (
+--   id INTEGER PRIMARY KEY,
+--   first_name TEXT,
+--   last_name TEXT,
+--   bats TEXT,
+--   throws TEXT
+-- );
+-- CREATE TABLE stats (
+--   id INTEGER PRIMARY KEY,
+--   team_id INTEGER,
+--   player_id INTEGER,
+--   games INTEGER,
+--   at_bats INTEGER,
+--   runs INTEGER,
+--   hits INTEGER,
+--   doubles INTEGER,
+--   triples INTEGER,
+--   home_runs INTEGER,
+--   rbis INTEGER
+-- );
